90f2e9ea1b502543c1ae54cf000bdbb349eb584d
Sudipta Mandal <146841766+imsudiptaa@users.noreply.github.com>
2025-08-03 01:04:47 +0530

    UI update

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -6,12 +6,12 @@ export const homeCarousel = [
 ]
 
 export const shopCategories = [
-  {name:'Art',url:'https://cdn.cdnparenting.com/articles/2019/05/18105353/72701002-H.jpg'}, 
-  {name:'Pottery',url:'https://tse1.mm.bing.net/th/id/OIP.w5D17DjUHQvZ-mVzF6V-yAHaFj?r=0&rs=1&pid=ImgDetMain&o=7&rm=3'}, 
-  {name:'Crafts',url:'https://tse2.mm.bing.net/th/id/OIP.fzrG7FVd5u3M_NV85A-EDgHaG1?r=0&rs=1&pid=ImgDetMain&o=7&rm=3'}, 
-  {name:'Crochet',url:'https://cdn11.bigcommerce.com/s-fhnzla4epa/images/stencil/500x659/products/2511/3841/Crocheted_Picot_Doily_Pattern___40770.1496534726.jpg?c=2'}, 
-  {name:'Fashion',url:'https://i.etsystatic.com/33115522/r/il/1fa02b/4368150524/il_fullxfull.4368150524_qhda.jpg'}, 
-  {name:'Accessories',url:'https://i.pinimg.com/originals/8e/1a/e2/8e1ae237ff1892174fa3f6a604d4b574.jpg'}
+  {name:'Art',url:'https://res.cloudinary.com/dmljao4pk/image/upload/v1754161213/Art_kahoi1.png'}, 
+  {name:'Pottery',url:'https://res.cloudinary.com/dmljao4pk/image/upload/v1754161213/Pottery_gjeqpp.png'}, 
+  {name:'Crafts',url:'https://res.cloudinary.com/dmljao4pk/image/upload/v1754161213/Crafts_lninlj.png'}, 
+  {name:'Crochet',url:'https://res.cloudinary.com/dmljao4pk/image/upload/v1754161213/Crochet_xbqaqa.png'}, 
+  {name:'Fashion',url:'https://res.cloudinary.com/dmljao4pk/image/upload/v1754161214/Fashion_uug29b.png'}, 
+  {name:'Accessories',url:'https://res.cloudinary.com/dmljao4pk/image/upload/v1754161212/Accessories_cxc9tb.png'}
 ]
 
 export const shopStates = [
